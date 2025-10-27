2106300679bdfadbc26328cf25494d2459b39770
jalleon <sachagalea@gmail.com>
2025-10-26 21:58:05 -0400

    fix: explicitly expose NEXT_PUBLIC_GOOGLE_MAPS_API_KEY in next.config.js for production

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withNextIntl = require('next-intl/plugin')('./i18n.ts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
   /* config options here */
 }
 
